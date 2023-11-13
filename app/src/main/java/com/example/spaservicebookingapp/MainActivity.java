0e9c236acfc22bc0d285f64a57a0b2290f717d36
Fajarbaluch63 <fajarbaluch63@gmail.com>
2023-11-13 23:34:25 +0000

    First Activity startup

--- app/src/main/java/com/example/spaservicebookingapp/MainActivity.java
+++ app/src/main/java/com/example/spaservicebookingapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.spaservicebookingapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set up the toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Configure the search view
+        SearchView searchView = findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Handle search query submission here
+                // Perform search or update the UI accordingly
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Handle search text change here
+                // You can filter/search the list as the user types
+                return false;
+            }
+        });
+
+        // Additional setup for RecyclerView, BottomNavigationView, etc.
     }
 }
